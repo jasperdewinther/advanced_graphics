d1f16078988da26297a85a99099566ebeef0f4c1
jasperdewinther <31817802+jasperdewinther@users.noreply.github.com>
2021-11-22 17:14:10 +0100

    added additional logging

--- myapp.cpp
+++ myapp.cpp
@@ -1,6 +1,8 @@
 #include "precomp.h"
 #include "myapp.h"
 #include "Ray.h"
+#include "Utils.h"
+#include <format>
 
 TheApp* CreateApp() { return new MyApp(); }
 
@@ -21,7 +23,8 @@ void MyApp::Tick( float deltaTime )
 	float3 camera_pos = float3(0, 0, 0);
 	float3 camera_dir = float3(sin(total_time / 10), 0, cos(total_time / 10));
 	std::vector<Ray> rays = generate_primary_rays(camera_pos, camera_dir, 90, screen->width, screen->height);
-	printf("time: %f\n", deltaTime);
+	printf("time: %f dir: %f %f %f\n", deltaTime, camera_dir.x, camera_dir.y, camera_dir.z);
+	
 	// clear the screen to black
 	screen->Clear( 0 );
 	// print something to the console window
@@ -36,6 +39,11 @@ void MyApp::Tick( float deltaTime )
 		uint blue = (uint)(r.d.z*255.0f);
 		screen->Plot( x, y, (red << 16) + (green << 8) + blue );
 	}
+	printf("top left -- %f %f %f\n", rays[0].d.x, rays[0].d.y, rays[0].d.z);
+	printf("top rigt -- %f %f %f\n", rays[screen->width - 1].d.x, rays[screen->width - 1].d.y, rays[screen->width - 1].d.z);
+	printf("bot left -- %f %f %f\n", rays[screen->height * (screen->width - 1) + 1].d.x, rays[screen->height * (screen->width - 1) + 1].d.y, rays[screen->height * (screen->width - 1) + 1].d.z);
+	printf("top rigt -- %f %f %f\n", rays[screen->height * screen->width - 1].d.x, rays[screen->height * screen->width - 1].d.y, rays[screen->height * screen->width - 1].d.z);
+
 	// plot a white pixel in the bottom right corner
 	screen->Plot( SCRWIDTH - 2, SCRHEIGHT - 2, 0xffffff );
 }
